9472c38dc8677a8046f23013c8b191c3d6f6c3e6
eatean <eat3an@protonmail.com>
2026-02-10 19:34:50 -0800

    adasfasdf

---
+++ swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  spaceBetween: 0,
+  grabCursor: true,
+  autoHeight: true,
+  loop: true,
+  mousewheel: {
+    invert: true,
+  },
+});
